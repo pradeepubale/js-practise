1ef5abf7a29e48dc109a4babc813d60756326383
Pradeep Ubale Patil <pradeepubale@users.noreply.github.com>
2024-07-24 11:25:15 +0000

    arithmatic operations study

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -32,5 +32,50 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 32
 let strNumber = String(someNumber)
 
-console.log(strNumber);
-console.log(typeof strNumber);
+// console.log(strNumber);
+// console.log(typeof strNumber);
+
+// **********************Operations *******
+
+let value = 3
+let negVal = -value
+
+// console.log(negVal);
+// console.log(typeof negVal);
+
+/**
+ * Arithmatic Operations
+ */
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+let str1 = "Hello "
+let str2 = "world"
+let str3 = str1 + str2
+
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // 122 if first is string then others also converted in string
+// console.log(1 + 2 + "2"); //32
+// console.log( ((3 + 7) * 9) / 5); // use parenthesis for readability
+
+
+// console.log(+true); // never do it rubbish way =>1
+// console.log(+""); // don't do it silly => 0
+
+// Radability is very Imp in coding
+let no1, no2, no3
+no1 = no2 = no3 = 2+2 // stupid way
+
+let gameCounter = 100
+//gameCounter++; // 101
+++gameCounter; // 101
+console.log(gameCounter);
+
+// study pre and post ++ operators on mdn
