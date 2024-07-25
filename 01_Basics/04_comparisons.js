8dc5dc2a0e3eed178776fe863ced8e714f85ad2e
Pradeep Ubale Patil <pradeepubale@users.noreply.github.com>
2024-07-25 07:14:14 +0000

    comparisons in js detail

--- 01_Basics/04_comparisons.js
+++ 01_Basics/04_comparisons.js
@@ -7,6 +7,8 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
+
+// here 👇 is problem when datatypes of values becomes change
 // console.log("2" > 1); // here string is converted implicitely(automatically) to number
 // console.log("02" > 1); // here also
 
@@ -24,11 +26,11 @@
 // console.log(undefined < 0);
 // console.log(undefined > 0);
 
-// never used above 👆 comparisons; use clean code
+// never used above 👆 comparisons(Avoid these); use clean code
 
 console.log(null == undefined);
 console.log(null > undefined);
 
-// compare value with data type
+// compare value with data type (===) i.e. strict check
 console.log("2" == 2); // true
 console.log("2" === 2); // false
