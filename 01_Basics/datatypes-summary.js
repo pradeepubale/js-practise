95525649c71ce365cacca3f005054b2469316fad
Pradeep Ubale Patil <pradeepubale@users.noreply.github.com>
2024-07-25 07:51:31 +0000

    datatypes

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,50 @@
+/**  Difference of data types is made on the basis of how they stored in memory and how we access the data.
+ * Two types Primitive
+ * and Non Primitive or reference type
+ * 
+ * Remember JS is dynamically typed language
+*/
+
+// Primitive call by value changes made on the copy instead of original
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt(for scientific)
+
+const name = "Pradeep"
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let user; // undefined
+
+// Symbol
+const id = Symbol('abc') // creates unique symbol
+const anotherId = Symbol('abc')
+
+// console.log(id === anotherId); // false
+
+// bigint
+const bigNumber = 1234567896543215684n
+
+
+// Reference or non primitive
+
+// Array, Objects, Functions
+
+//arrays
+const computer = ["cpu", "keyboard", "mouse", "monitor"];
+
+// objects
+/**
+ * whatever we define within {} brackets is object
+ * datatype can be anything string, number, boolean, 
+ * array, function or even object also.
+ */
+let myObj = {
+    name: "ABC",
+    age: 25,
+}
+
+// lets define a function as a variable
+const myFunction = function(){
+    console.log("Hello World");
+}
