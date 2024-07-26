ed325c1451c7fe8d087ba6a9c846488411f07d6b
Pradeep Ubale Patil <pradeepubale@users.noreply.github.com>
2024-07-26 07:41:41 +0000

    dates

---
+++ 01_Basics/08_date.js
@@ -0,0 +1,20 @@
+/**
+ * Date has been a long-standing pain point in ECMAScript
+ * Documentation Link:
+ * https://tc39.es/proposal-temporal/docs/
+ */
+
+
+// dates in JS this time
+
+let myDate = new Date()
+
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
